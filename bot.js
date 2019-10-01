0c908f6f130f001422e1f7402a0651962b2ebd15
Shoukomoe <39826024+Shoukomoe@users.noreply.github.com>
2019-10-01 16:34:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = "#";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.Activity('Black Clover 103 on HerkuAnime', { type: 'WATCHING' })
+client.user.setPresence({ game: { name: 'Black Clover 103 on HerokuAnime' , type: 'WATCHING' }, status: 'idle' })
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
