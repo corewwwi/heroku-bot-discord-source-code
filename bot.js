1ce9c8b59ea8aa319920c87fba42985e2740343b
Shoukomoe <39826024+Shoukomoe@users.noreply.github.com>
2019-10-02 00:15:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = "#";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setPresence({ game: { name: 'Try Knights - 10' , type: 'WATCHING' }, status: 'idle' })
+client.user.setPresence({ game: { name: 'Members' , type: 'WATCHING' }, status: 'dnd' })
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
