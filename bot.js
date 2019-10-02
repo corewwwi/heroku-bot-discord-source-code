1701ccea2820a48846d4cf4d336e87a66d05c348
Shoukomoe <39826024+Shoukomoe@users.noreply.github.com>
2019-10-02 17:47:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = "#";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setPresence({ game: { name: 'Ahiru no Sora' , type: 'WATCHING' }, status: 'dnd' })
+client.user.setPresence({ game: { name: 'Kono Yo no Hate de Koi' , type: 'WATCHING' }, status: 'dnd' })
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
