057b2d70df926a5c4fec35b18d888a6af4762741
Shoukomoe <39826024+Shoukomoe@users.noreply.github.com>
2019-10-04 19:45:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = "#";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setPresence({ game: { name: 'Null Peta - 01' , type: 'WATCHING' }, status: 'dnd' })
+client.user.setPresence({ game: { name: 'Granblue Fantasy The Animation - 01' , type: 'WATCHING' }, status: 'dnd' })
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
